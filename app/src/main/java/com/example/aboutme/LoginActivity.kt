13a71184fc9b3a79ab92bd6e14cc30b69bdcc562
Shubham Garg <shubhamgarg1671@gmail.com>
2021-10-14 20:25:04 +0530

    minor changes

--- app/src/main/java/com/example/aboutme/LoginActivity.kt
+++ app/src/main/java/com/example/aboutme/LoginActivity.kt
@@ -1,13 +1,32 @@
 package com.example.aboutme
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
-import android.widget.*
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 class LoginActivity : AppCompatActivity() {
+    private val PICK_FROM_GALLARY = 2
+    lateinit var addProfileImage:ImageView
+    lateinit var LoginCompletedButton:Button
+    private lateinit var database:FirebaseDatabase
     private lateinit var auth: FirebaseAuth
     lateinit var signInProgress:ProgressBar
     val TAG = "logInActivity"
@@ -17,8 +36,66 @@ class LoginActivity : AppCompatActivity() {
         val storage = Firebase.storage
         // Create a storage reference from our app
         val storageRef = storage.reference
-        // Points to "images"
-        val imagesRef = storageRef.child("images")
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+        addProfileImage = findViewById(R.id.addProfileImage)
+        LoginCompletedButton = findViewById(R.id.LoginCompletedButton)
+        LoginCompletedButton.setOnClickListener {
+            // Get the data from an ImageView as bytes
+            addProfileImage.isDrawingCacheEnabled = true
+            addProfileImage.buildDrawingCache()
+            val bitmap = (addProfileImage.drawable as BitmapDrawable).bitmap
+            val baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+            // Points to "images"
+            val imagesRef = storageRef.child("image/${auth.uid}/profilePicture")
+            val uploadTask = imagesRef.putBytes(data)
+                uploadTask.addOnFailureListener {
+                Log.d(TAG, "onCreate() called $it")
+             // Handle unsuccessful uploads
+            }.addOnSuccessListener { taskSnapshot ->
+                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                // ...
+                imagesRef.downloadUrl.addOnCompleteListener { task ->
+                        val profileImageUrl = task.result.toString()
+                        Log.i("URL", profileImageUrl)
+                    val myref = database.getReference("user/${auth.uid}/profileImage")
+                    myref.setValue(profileImageUrl)
+                }
+                .addOnFailureListener { e ->
+                    Log.d(TAG, "onCreate() called with: e = $e")
+                }
+            }
+        }
+        addProfileImage.setOnClickListener {
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galleryIntent, PICK_FROM_GALLARY)
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_FROM_GALLARY) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    val imageUri: Uri? = data?.getData()
+                    val imageStream: InputStream? = imageUri?.let {
+                        contentResolver.openInputStream(
+                            it
+                        )
+                    }
+                        val selectedImage = BitmapFactory.decodeStream(imageStream)
+                    addProfileImage.setImageBitmap(selectedImage)
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
