91fd2f809f0caebe43d47e956fa565c827d55265
Shubham Garg <shubhamgarg1671@gmail.com>
2021-10-18 12:04:27 +0530

    some changes

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -20,13 +20,13 @@ class MainActivity : AppCompatActivity() {
         //val database = FirebaseDatabase.getInstance()
         val storage = FirebaseStorage.getInstance()
         // ImageView in your Activity
-        val imageView = findViewById<ImageView>(R.id.imageview)
+//        val imageView = findViewById<ImageView>(R.id.imageview)
         val storageReference: StorageReference = storage.reference
         val photoReference:StorageReference = storageReference.child("image/bu5bUwzdJFc634rbBW2RKAwlfOI3/profilePicture")
         val ONE_MEGABYTE = (1024 * 1024).toLong()
         photoReference.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
             val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-            imageView.setImageBitmap(bmp)
+//            imageView.setImageBitmap(bmp)
         }.addOnFailureListener {
             Log.e(TAG, "onCreate() called ${it.message}")
             Toast.makeText(
