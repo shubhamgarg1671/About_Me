85141161972255095254752edd7065f58ae5ffdd
Shubham Garg <shubhamgarg1671@gmail.com>
2021-10-20 22:10:54 +0530

    preview screen added

---
+++ app/src/main/java/com/example/aboutme/previewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class previewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+
+    }
+}
