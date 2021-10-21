21ddd3ca9766b389c8c4f20f85f0e19a215d5b4a
Shubham Garg <shubhamgarg1671@gmail.com>
2021-10-21 13:31:19 +0530

    some changes

--- app/src/main/java/com/example/aboutme/previewActivity.kt
+++ app/src/main/java/com/example/aboutme/previewActivity.kt
@@ -1,12 +1,47 @@
 package com.example.aboutme
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class previewActivity : AppCompatActivity() {
+    val TAG = "previewActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+        val uid = FirebaseAuth.getInstance().uid!!
+        val storage = FirebaseDatabase.getInstance()
+        val myRef = storage.getReference("user/$uid")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue()
+                Log.d(TAG, "Value is: $value")
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read value.", error.toException())
+            }
+        })
 
     }
+    fun redirecttoURL(url:String) {
+        val httpIntent = Intent(Intent.ACTION_VIEW)
+        httpIntent.data = Uri.parse(url)
+
+        startActivity(httpIntent)
+    }
+
 }
