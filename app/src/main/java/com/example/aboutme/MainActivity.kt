45775c06569929277bad9b80481d1632b7803897
Shubham Garg <shubhamgarg1671@gmail.com>
2021-10-29 11:31:36 +0530

    some changes

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -138,7 +138,31 @@ class MainActivity : AppCompatActivity() {
         }
         val addLinkedinButton:ImageView = findViewById(R.id.addLinkedinButton)
         addLinkedinButton.setOnClickListener {
-            dialogBoxwithEdittext("Linkedin","Add Profile Link")
+            val factory = LayoutInflater.from(this)
+            val dialogView: View = factory.inflate(R.layout.instagram_dialogue_box, null)
+            val dialogImageCardInsta:CardView = dialogView.findViewById(R.id.dialogImageCardInsta)
+            dialogImageCardInsta.bringToFront()
+            val dialog = AlertDialog.Builder(this).create()
+            dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+
+            dialog.setView(dialogView)
+            dialogView.findViewById<Button>(R.id.dialog_insta).setOnClickListener{
+                val instalink:EditText = dialogView.findViewById<EditText>(R.id.dialog_insta_link)
+                val link:String = instalink.text.toString()
+                if (Patterns.WEB_URL.matcher(link).matches()) {
+                    myRef = database.getReference("user/$uid/Instagram")
+                    myRef.setValue(link)
+                    Log.d(TAG, "onDialog $link")
+                    dialog.cancel()
+                } else {
+                    Toast.makeText(this, "Link is not correct", Toast.LENGTH_LONG).show()
+                }
+            }
+            dialogView.findViewById<TextView>(R.id.insta_dialog_cancel).setOnClickListener {
+                dialog.cancel()
+            }
+            dialog.show()
+//            dialogBoxwithEdittext("Linkedin","Add Profile Link")
         }
         val addMoreProfileButton:ImageView = findViewById(R.id.addMoreProfileButton)
         addMoreProfileButton.setOnClickListener {
