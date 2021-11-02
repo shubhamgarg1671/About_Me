b1d4aad042219300941aa78a321c45aaf1e1a31b
Shubham Garg <shubhamgarg1671@gmail.com>
2021-11-02 10:22:25 +0530

    some changes

--- app/src/main/java/com/example/aboutme/HomeScreen.kt
+++ app/src/main/java/com/example/aboutme/HomeScreen.kt
@@ -1,12 +1,26 @@
 package com.example.aboutme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
-
+        val edittextUid:EditText = findViewById(R.id.edittextUid)
+        val uid:String = edittextUid.text.toString()
+        val editProfile:Button = findViewById(R.id.editProfile)
+        editProfile.setOnClickListener {
+            val intent:Intent = Intent(this,previewActivity::class.java)
+            startActivity(intent)
+        }
+        val button:Button = findViewById(R.id.Button)
+        button.setOnClickListener {
+            val intent:Intent = Intent(this, previewActivity::class.java,)
+            startActivity(intent)
+        }
     }
 }
